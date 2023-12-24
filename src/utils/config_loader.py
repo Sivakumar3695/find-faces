ceb11adb3f27a14de0d069f71581ca9dfad1ae5d
Sivakumar <sivakumar.dpalanisamy@gmail.com>
2023-12-24 17:57:03 +0530

    Minor Fix

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -5,6 +5,5 @@ from pyaml_env import parse_config
 def load():
     config = parse_config(f'{os.getcwd()}/config.yml', default_value='')
     print(config)
-    print(config['app_config']['test'])
     print(config['app_config']['db_url'])
     return config['app_config']
